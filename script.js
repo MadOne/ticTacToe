b3fb7fd710507ca235e5dfd975131e8398f3c7a7
MadOne <madone85@googlemail.com>
2023-11-16 16:16:12 +0100

    add getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+
+const symbols = ["rock", "paper", "scissors"];
+let scoreComputer = 0;
+let scorePlayer = 0;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    return symbols[getRandomInt(3)];
+}
+
