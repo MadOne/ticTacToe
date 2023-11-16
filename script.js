7aa36afa635f3420839768910f1c9ca17d669484
MadOne <madone85@googlemail.com>
2023-11-16 16:37:40 +0100

    add game() and finish program

--- script.js
+++ script.js
@@ -21,28 +21,34 @@ function playRound(playerSelection, computerSelection){
     }
     if(playerSelection == "rock" && computerSelection == "paper") {
         console.log("You Lose! Paper beats Rock");
+        scoreComputer++;
     }
     if(playerSelection == "rock" && computerSelection == "scissors") {
         console.log("You Win! Rock smashes scissors");
+        scorePlayer++;
     }
 
     //player paper
     if(playerSelection == "paper" && computerSelection == "rock") {
         console.log("You Win! Paper wraps Rock");
+        scorePlayer++;
     }
     if(playerSelection == "paper" && computerSelection == "paper") {
         console.log("Draw!");
     }
     if(playerSelection == "paper" && computerSelection == "scissors") {
         console.log("You Loose! Scissors cut paper!");
+        scoreComputer++;
     }
 
     //player scissors
     if(playerSelection == "scissors" && computerSelection == "rock") {
         console.log("You Lose! Rock smashes Scissors");
+        scoreComputer++;
     }
     if(playerSelection == "scissors" && computerSelection == "paper") {
         console.log("You Win! Scissors cut Paper");
+        scorePlayer++;
     }
     if(playerSelection == "scissors" && computerSelection == "scissors") {
         console.log("Draw");
@@ -50,4 +56,22 @@ function playRound(playerSelection, computerSelection){
 }
 
 
-playRound("rock", "rock");
+function game() {
+    for(let i = 0; i < 5; i++) {
+        playerSelection = prompt("Whats your choice? rock, paper or scissors")
+        playRound(playerSelection, getComputerChoice())
+    }
+}
+
+game()
+
+if (scorePlayer > scoreComputer) {
+    console.log(`You won the game ${scorePlayer} / ${scoreComputer}`);
+}
+if (scorePlayer < scoreComputer) {
+    console.log(`You loose the game ${scorePlayer} / ${scoreComputer}`);
+}
+
+if (scorePlayer == scoreComputer) {
+    console.log(`The game is draw! ${scorePlayer} / ${scoreComputer}`);
+}
